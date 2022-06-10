edcc46397076b5cc085e3f495df49d8f6f67c898
Pedro Abreu <henriqueibtou@gmail.com>
2022-06-10 09:09:33 -0700

    Chapter 3 complete - Made the ring and gave it to Frodo.

--- js/app.js
+++ js/app.js
@@ -57,12 +57,21 @@ function makeHobbits() {
 	console.log('2: makeHobbits')
 
 	// display an `unordered list` of hobbits in the shire
-
+	const ul = document.createElement('ul')
 	// give each hobbit a class of `hobbit`
-
+	for (let i=0; i<hobbits.length; i++){
+		let li = document.createElement('li');
+		li.setAttribute('id', hobbits[i])
+		ul.appendChild(li)
+		li.innerText = hobbits[i]
+		console.log(li)
+	}
 	// hint: create a 'ul' outside the loop into which to append the 'li's
+	//const addressDiv =document.createElement('div')
+	document.getElementById(lands[0]).appendChild(ul)
 
 	// hint: get 'The-Shire' by using its id
+	
 }
 
 // COMMIT YOUR WORK
@@ -76,10 +85,16 @@ function keepItSecretKeepItSafe() {
 	console.log('3: keepItSecretKeepItSafe')
 
 	// create a div with an id of `'the-ring'`
-
+	const theRing =  document.createElement('div');
+	theRing.setAttribute('id', 'the-ring');
 	// give the div a class of `'magic-imbued-jewelry'`
+	theRing.setAttribute('class', 'magic-imbued-jewelry');
 
 	// add the ring as a child of `Frodo`
+	const frodo = document.getElementById('Frodo Baggins')
+	frodo.appendChild(theRing)
+	
+	
 }
 
 // COMMIT YOUR WORK
